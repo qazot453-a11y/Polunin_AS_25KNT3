c50b47d6dbeeeadf9dfdb4ed4804c4134e1f361a
Alexey Polunin <qazot453@gmail.com>
2025-09-26 11:13:21 +0300

    Вывод мат. операции

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-print('Здравствуйте, введите 2 целых числа: ')
-a,b = int(input()), int(input())
+print("Здравствуйте, введите 2 целых числа: ")
+a, b = int(input()), int(input())
+total = a + b
+print(total)
